b9cb3b1f94f95b60c23c76f7767f13ab01d82d86
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-09-13 14:53:00 -0400

    done

--- assets/js/game.js
+++ assets/js/game.js
@@ -126,12 +126,19 @@ var startGame = function() {
 
 // function to end entire game
 var endGame = function() {
-    // if player is still alive, they win
-    if (playerInfo.health > 0 ) {
-    window.alert("Great job, you survived the game! Your new score is " + playerInfo.money + ".");
-    } 
-    else {
     window.alert("The game has now ended. Let's see how you did!");
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }  
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
     
 var playAgainConfirm = window.confirm("Would you like to play again?");
